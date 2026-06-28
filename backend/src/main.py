5effdbe0e9efee6123c6dd7ad0c8f0ef41fb8f2c
HiteshhYadav <yadavhitesh144@gmail.com>
2026-06-28 21:51:01 +0530

    update

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,15 @@
+import os 
 from fastapi import FastAPI 
 
 
 app=FastAPI()
 
+MY_PROJECT=os.environ.get("MY_PROJECT") or "This is my project" #fallback
+API_KEY=os.environ.get("API_KEY")
+
+if not API_KEY:
+    raise NotImplementedError("'API_KEY' is not set!")
+
 @app.get("/")
 def read_index():
-    return {"hello":"world again!"} 
+    return {"hello":"world again!","project-name":MY_PROJECT,"secret-api-key":API_KEY} 
